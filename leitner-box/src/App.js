07ff60a65f383765e36e4180afce9e81a2745fdb
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-01-15 19:26:58 +0330

    create fiest page

--- leitner-box/src/App.js
+++ leitner-box/src/App.js
@@ -1,9 +1,14 @@
 import '../src/styles/app.scss';
+import FirstPage from '../src/components/FirstPage'
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="pages">
+      <Routes>
+        <Route exact path="/" element={<FirstPage />}></Route>
+      </Routes>
+
     </div>
   );
 }
