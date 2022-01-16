a738acb16a400d88357e9717d37ba7a5b656d613
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-01-16 12:53:47 +0330

    edit some codes

--- leitner-box/src/App.js
+++ leitner-box/src/App.js
@@ -1,12 +1,19 @@
 import '../src/styles/app.scss';
 import FirstPage from '../src/components/FirstPage'
 import { Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
+import Words from '../src/data/words';
+import Learn from '../src/components/Learn'
 
 function App() {
+
+  const[words, setWords] = useState(Words());
+
   return (
     <div className="pages">
       <Routes>
-        <Route exact path="/" element={<FirstPage />}></Route>
+        <Route exact path="/" element={<FirstPage words = {words} />}></Route>
+        <Route exact path="/Learn" element={<Learn />}></Route>
       </Routes>
 
     </div>
--- leitner-box/src/components/FirstPage.js
+++ leitner-box/src/components/FirstPage.js
@@ -1,12 +1,17 @@
 import {Link} from 'react-router-dom';
 
-const FirstPage = () => {
+
+const FirstPage = (props) => {
+
+    const storeData = () =>{
+        localStorage.setItem('words', JSON.stringify(props.words));
+    }
 
     return (
         <div className="container">
             <h1>زبان آموز</h1>
             <p>.وبسایت زبان آموز بستری برای یادگیری زبان انگلیسی بر پایه جعبه لایتنر میباشد</p>
-            <Link className="link" to="/Learn">بزن بریم</Link>
+            <Link onClick={() => storeData()} className="link" to="/Learn">بزن بریم</Link>
         </div>
     )
 
