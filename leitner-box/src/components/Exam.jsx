926741ba0e045018b33369fa63a692bb7e5d6cbe
Reza javan <87618089+rezajavan1380@users.noreply.github.com>
2022-01-19 12:05:49 +0330

    create exam file

--- leitner-box/src/components/Exam.jsx
+++ leitner-box/src/components/Exam.jsx
@@ -1,16 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
+import moment from 'moment';
 import Question from './Question';
 
-const Exam = () => {
+const Exam = (props) => {
 
-    // const [controller, setController] = useState(0);
-    // const [words, setWords] = useState(JSON.parse(localStorage.getItem(('todayWords'))));
-    // Word={words[controller]} answer={words.map((word) => word.persian)} setController = {setController}
+     const [controller, setController] = useState(0);
+
+    const[todayWords, setTodayWords] = useState([]);
+
+    useLayoutEffect(() => {
+
+        let count = 0;
+        const nullWords = [];
+
+
+        while (count < 30) {
+            if (nullWords.length === 0) {
+                for (let i = 0; i < props.words.length; i++) {
+                    if (props.words[i].nextDate === null) {
+                        nullWords.push(props.words[i]);
+                    }
+                }
+            }
+            setTodayWords([...todayWords, todayWords.push(nullWords.shift())]);
+            count++;
+        }
+        const today = moment().format("YYYY-MM-DD");
+
+        for (let i = 0; i < props.words.length; i++) {
+            if (today >= props.words[i].nextDate) {
+                setTodayWords([...todayWords, todayWords.push(props.words[i])]);
+            }
+        }
+    }, [])
     
     return (
         <div>
             <div className="examContainer">
-                <Question Word="bvbbv" />
+                <Question todayWords={todayWords} controller={controller} setController={setController} />
             </div>
         </div>
     );
--- leitner-box/src/components/Question.jsx
+++ leitner-box/src/components/Question.jsx
@@ -1,40 +1,60 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect, Fragment } from 'react';
 
-class Question extends Component {
-    render() {
-        // const {Word, answer, setController} = this.props;
-        return (
-            <div className='questionContainer'>
-                <h1>test</h1>
-                <hr  />   
-                <div className='choosebox'>
-                    <label className="labl">
-                        <input type="radio" name="radioname" value="another" />
-                        <div>Small</div>
-                    </label>
-                    <label className="labl">
-                        <input type="radio" name="radioname" value="another" />
-                        <div>Small</div>
-                    </label>
-                    <br/>
-                    <label className="labl">
-                        <input type="radio" name="radioname" value="another" />
-                        <div>Small</div>
-                    </label>
-                    <label className="labl">
-                        <input type="radio" name="radioname" value="another" />
-                        <div>Small</div>
-                    </label>
-                </div> 
-                <div className='buttonsDiv'>
-                    <Link className='buttons' to="/Learn/Exam">قبلی</Link>
-                    <Link className='buttons' to="/Learn/Exam">بعدی</Link>
-                </div>
-                
-            </div>
-        );
+function Question (props) {
+    
+    const answers = [];
+    const [eWord, setEWord] = useState();
+
+
+    const NOP = (type) => {
+        if(type === 'next'){
+            props.setController(props.controller + 1);
+            setEWord(props.todayWords[props.controller].english);
+        }
+        else if(type === 'prev'){
+            props.setController(props.controller - 1);
+            setEWord(props.todayWords[props.controller].english);
+        }
     }
+
+    useEffect(() => {
+
+        setEWord(props.todayWords[props.controller].english);
+            
+        let currentNum = Math.floor(Math.random() * 4) + 1;
+        let mistakeNum = Math.floor(Math.random() * 25) + 1; //random number between 1 and 26
+
+        for(let i = mistakeNum; i < (mistakeNum + 3); i++){
+            answers.push(props.todayWords[i].persian);
+        }
+
+        answers.splice(currentNum, 0, props.todayWords[props.controller].persian);
+
+        console.log(answers);
+
+    }, [])
+
+    return (
+        <div className='questionContainer'>
+            <h1>{eWord}</h1>
+            <hr  />   
+            <div className='choosebox'>
+                {answers.map((answer, index) => {
+                    return <Fragment key={index}>
+                        <div>
+                            <div>{answer}</div>
+                        </div>
+                    </Fragment>
+
+                })}
+            </div> 
+            <div className='buttonsDiv'>
+                <button onClick={() => NOP('next')} className='buttons'>قبلی</button>
+                <button onClick={() => NOP('prev')} className='buttons'>بعدی</button>
+            </div>
+            
+        </div>
+    );
 }
 
 export default Question;
