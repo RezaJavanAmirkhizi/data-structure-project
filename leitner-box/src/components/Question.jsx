1a64d3a0d7a1519f30ce12fda51da045b7a1e67b
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-01-19 17:39:41 +0330

    commit 2

--- leitner-box/src/components/Question.jsx
+++ leitner-box/src/components/Question.jsx
@@ -1,40 +1,90 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 
-class Question extends Component {
-    render() {
-        // const {Word, answer, setController} = this.props;
-        return (
-            <div className='questionContainer'>
-                <h1>test</h1>
-                <hr  />   
-                <div className='choosebox'>
-                    <label className="labl">
-                        <input type="radio" name="radioname" value="another" />
-                        <div>Small</div>
-                    </label>
-                    <label className="labl">
-                        <input type="radio" name="radioname" value="another" />
-                        <div>Small</div>
-                    </label>
-                    <br/>
-                    <label className="labl">
-                        <input type="radio" name="radioname" value="another" />
-                        <div>Small</div>
-                    </label>
-                    <label className="labl">
-                        <input type="radio" name="radioname" value="another" />
-                        <div>Small</div>
-                    </label>
-                </div> 
-                <div className='buttonsDiv'>
-                    <Link className='buttons' to="/Learn/Exam">قبلی</Link>
-                    <Link className='buttons' to="/Learn/Exam">بعدی</Link>
-                </div>
-                
-            </div>
-        );
+import React, { useState, useEffect, Fragment } from 'react';
+
+function Question(props) {
+
+    const [answers, setAnswers] = useState([]);
+    const [todayWords, setTodayWords] = useState(props.todayWords)
+    const [eWord, setEWord] = useState();
+    const [controller, setController] = useState(0)
+
+
+    const NOP = (type) => {
+        if (type === 'next') {
+            setController(controller + 1);
+            setEWord(todayWords[controller + 1].english);
+        }
+        {/*else if (type === 'prev') {
+            setController(controller - 1);
+            setEWord(todayWords[controller - 1].english);
+        }*/}
     }
+
+    useEffect(() => {
+
+        setTodayWords(props.todayWords)
+        setEWord(todayWords[controller].english);
+
+        let currentNum = Math.floor(Math.random() * 4) + 1;
+        let mistakeNum = Math.floor(Math.random() * 25) + 1; //random number between 1 and 26
+
+        let value = []
+        for (let i = mistakeNum; i < (mistakeNum + 3); i++) {
+            value.push(todayWords[i].persian);
+        }
+
+        console.log(value);
+        value.splice(currentNum, 0, todayWords[controller].persian);
+        setAnswers(value)
+
+        console.log(answers);
+
+    }, [])
+
+    useEffect(() => {
+        let currentNum = Math.floor(Math.random() * 4) + 1;
+        let mistakeNum = Math.floor(Math.random() * 25) + 1; //random number between 1 and 26
+
+        let value = []
+        for (let i = mistakeNum; i < (mistakeNum + 3); i++) {
+            value.push(todayWords[i].persian);
+        }
+
+        console.log(value);
+        value.splice(currentNum, 0, todayWords[controller].persian);
+        setAnswers(value)
+
+        console.log(answers);
+
+
+    }, [controller])
+
+    return (
+        <div className='questionContainer'>
+            <h1>{eWord}</h1>
+            <hr />
+            <div className='choosebox'>
+                {answers.map((answer, index) => {
+                    return <Fragment key={index}>
+                        <div style={{ color: 'red' }}>
+                            {answer}
+                        </div>
+                    </Fragment>
+                })}
+            </div>
+            <div className='buttonsDiv'>
+                {/* {controller === 0 ? ''
+                    :
+                    <button onClick={() => NOP('prev')} className='buttons'>قبلی</button>
+                } */}
+                {controller === (todayWords.length - 1) ? ''
+                    :
+                    <button onClick={() => NOP('next')} className='buttons'>بعدی</button>
+                }
+            </div>
+        </div>
+    );
 }
+
 
 export default Question;
