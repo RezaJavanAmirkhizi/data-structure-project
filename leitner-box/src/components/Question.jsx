c249d12b87c28d6f1350656f871d504a52c5fa17
Alireza-Al <90038814+Alireza-Al@users.noreply.github.com>
2022-01-20 22:23:34 +0330

    nothing special

--- leitner-box/src/components/Question.jsx
+++ leitner-box/src/components/Question.jsx
@@ -3,7 +3,6 @@ import moment from 'moment';
 
 function Question(props) {
 
-    const [today, setToday] = useState(null)
     const [answers, setAnswers] = useState([]);
     const [todayWords] = useState(props.todayWords)
     const [eWord, setEWord] = useState();
@@ -13,8 +12,8 @@ function Question(props) {
     const [message, SetMessage] = useState('');
 
     useEffect(() => {
-        setEWord(todayWords[controller].english);
 
+        setEWord(todayWords[controller].english);
 
         let currentNum = Math.floor(Math.random() * 4);
         let mistakeNum = Math.floor(Math.random() * 25) + 1; //random number between 1 and 26
@@ -59,10 +58,6 @@ function Question(props) {
         }
     }
 
-    const ExamMotherFucker = () => {
-        setToday(moment().add(1, "days").format('YYYY-MM-DD'))
-    }
-
     const checkQuestion = (question) => {
 
         if (question === answers[currectAnswer]) {
@@ -87,49 +82,60 @@ function Question(props) {
     return (
         <div className='questionContainer'>
 
-            {controller === (todayWords.length - 1)
-                    ?
-                    <div>
-                        <span style={{ color: 'green' }}>
-                            Currect Answers : {curretCounter}
-                        </span><br />
-                        <span style={{ color: 'red' }}>
-                            Wrong Answers : {todayWords.length - curretCounter}
-                        </span><br />
-                        <span style={{ color: 'white' }}>
-                            you answered around {((curretCounter / todayWords.length) * 100).toFixed(0)}% of the answers currectlly
-                        </span>
-                    </div>
-                    : 
-                    <><h1>{controller === (todayWords.length - 1) ? ''
-                    : eWord}</h1><hr /><div className='choosebox'>
-                        {answers.map((answer, index) => {
-                            return <Fragment key={index}>
-                                <label className="labl">
-                                    <input type="radio" name="radioname" value={answer} />
-                                    <div onClick={() => {
-                                        if (index === currectAnswer) {
-                                            setCurretCounter(curretCounter + 1);
-                                            SetMessage('correct');
-                                        }
-                                        else {
-                                            SetMessage('wrong');
-                                        }
-                                        setTimeout((() => {
-                                            NOP('next');
-                                            checkQuestion(answer);
-                                            SetMessage('');
-                                        }), '500');
-                                    } }>
-                                        {controller === (todayWords.length - 1) ? '' : answer}
-                                    </div>
-                                </label>
-                            </Fragment>;
-                        })}
-                    </div><div className='messageStyle' style={{ color: 'white', fontSize: '30px' }}>
-                        {message}
-                    </div></>
+            <div>
+                {
+                    controller === (todayWords.length - 1)
+                        ?
+                        setTimeout(
+                            (() => {
+                                <div>
+                                    <span style={{ color: 'green' }}>
+                                        Currect Answers : {curretCounter}
+                                    </span><br />
+                                    <span style={{ color: 'red' }}>
+                                        Wrong Answers : {todayWords.length - curretCounter}
+                                    </span><br />
+                                    <span style={{ color: 'white' }}>
+                                        you answered around {((curretCounter / todayWords.length) * 100).toFixed(0)}% of the answers currectlly
+                                    </span>
+                                </div>
+                            }), '5000')
+                        :
+                        <><h1>
+                            {eWord}
+                        </h1><hr />
+                            <div className='choosebox'>
+                                {answers.map((answer, index) => {
+                                    return <Fragment key={index}>
+                                        <label className="labl">
+                                            <input type="radio" name="radioname" value={answer} />
+                                            <div onClick={() => {
+                                                if (index === currectAnswer) {
+                                                    setCurretCounter(curretCounter + 1);
+                                                    SetMessage('correct');
+                                                }
+                                                else {
+                                                    SetMessage('wrong');
+                                                }
+                                                setTimeout((() => {
+                                                    NOP('next');
+                                                    checkQuestion(answer);
+                                                    SetMessage('');
+                                                }), '500');
+                                            }}>
+                                                {answer}
+                                            </div>
+                                        </label>
+                                    </Fragment>;
+                                })}
+                            </div>
+                            <div className='messageStyle' style={{ color: 'white', fontSize: '30px' }}>
+                                {message}
+                            </div>
+                        </>
                 }
+            </div>
+
         </div>
     );
 }
