8197d3732c4398432ad30be336d13b2d7eb2c5d3
Reza Javan <javan.reza1380@gmail.com>
2022-07-09 16:41:07 +0430

    Remove aqulats

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
-import '../src/styles/app.scss';
-import FirstPage from '../src/components/FirstPage'
-import { Route, Routes } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import Words from '../src/data/words';
-import SecondPage from './components/SecondPage'
-import Exam from './components/Exam';
-import Learn from './components/Learn';
+import "../src/styles/app.scss";
+import FirstPage from "../src/components/FirstPage";
+import { Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Words from "../src/data/words";
+import SecondPage from "./components/SecondPage";
+import Exam from "./components/Exam";
+import Learn from "./components/Learn";
 
 function App() {
+	const [words, setWords] = useState(
+		JSON.parse(localStorage.getItem("words")) === null
+			? Words()
+			: JSON.parse(localStorage.getItem("words"))
+	);
 
-  const[words, setWords] = useState(JSON.parse(localStorage.getItem('words')) === null ? Words() : JSON.parse(localStorage.getItem('words')));
-
-
-  return (
-    <div className="pages">
-      <Routes>
-        <Route exact path="/" element={<FirstPage words = {words} setWords={setWords} />}></Route>
-        <Route exact path={"/secondpage"} element={<SecondPage/>}/>
-        <Route path={"/secondpage/exam"} element={<Exam words={words} />} />
-        <Route path='/secondpage/learn' element={<Learn words={words}/>}/>
-      </Routes>
-
-    </div>
-
-
-  );
+	return (
+		<div className="pages">
+			<Routes>
+				<Route
+					exact
+					path="/"
+					element={<FirstPage words={words} setWords={setWords} />}
+				></Route>
+				<Route exact path="/secondpage" element={<SecondPage />} />
+				<Route
+					path="/secondpage/exam"
+					element={<Exam words={words} />}
+				/>
+				<Route
+					path="/secondpage/learn"
+					element={<Learn words={words} />}
+				/>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
